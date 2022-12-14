d170c467efe10df53b76034a3f951b0d8cad4316
zahiahmed <zahi.ahmed33@gmail.com>
2022-12-14 17:08:45 -0500

    finished phase 4

--- routes_controllers/app/controllers/users_controller.rb
+++ routes_controllers/app/controllers/users_controller.rb
@@ -14,6 +14,17 @@ class UsersController < ApplicationController
         end
     end
 
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        render json: @user
+    end
+
     def destroy
         @user = User.find(params[:id])
         @user.destroy
