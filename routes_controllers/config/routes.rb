b492d71e9a4cb9764ad3ff3f5ab53339333e9d22
Kevin Liu <kliu33@buffalo.edu>
2022-12-15 11:55:26 -0500

    finish yesterday project

--- routes_controllers/config/routes.rb
+++ routes_controllers/config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
-  # resources :users
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :artworks, only: [:index, :show, :create, :update, :destroy]
+  resources :artwork_shares, only: [:create, :destroy]
+
+  resources :users do
+    resources :artworks, only: [:index]
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  # # root "articles#index"
 
-  get 'users', to: 'users#index'
-  post '/users', to: 'users#create'
-  get '/users/new', to: 'users#new', as: 'new_user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
-  get '/users/:id', to: 'users#show', as: 'user'
-  patch '/users/:id', to: 'users#update'
-  put '/users/:id', to: 'users#update'
-  delete '/users/:id', to: 'users#destroy'
+  # get 'users', to: 'users#index'
+  # post '/users', to: 'users#create'
+  # # get '/users/new', to: 'users#new', as: 'new_user'
+  # # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # patch '/users/:id', to: 'users#update'  
+  # # put '/users/:id', to: 'users#update'
+  # delete '/users/:id', to: 'users#destroy'
 
 
 end
